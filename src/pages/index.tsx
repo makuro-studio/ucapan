4a8bf1ec2e243624a315677235a5f10cd97ef230
bipproduction <bip.production.js@gmail.com>
2023-04-22 10:13:07 +0800

    tambah keterangan og

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import {Image} from '@mantine/core'
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Image } from "@mantine/core";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -13,8 +13,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:image" content="/kartu.png" />
+        <meta property="og:title" content="Minal Aidzin Wal Faidzin" />
+        <meta
+          property="og:description"
+          content="Malik Sekeluarga Menucapkan Minal Aidzin Wal Faidzin , Mohon Maaf Lahir Dan Batin"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
-      <Image src={'/kartu.png'} alt='kartu' />
+      <Image src={"/kartu.png"} alt="kartu" />
     </>
-  )
+  );
 }
